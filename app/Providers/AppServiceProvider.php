59d8c83ee4b4b20e56254a20f8d8bea118e209f7
rizalfahmi1 <39107426+rizalfahmi1@users.noreply.github.com>
2025-12-05 09:17:29 +0700

    appserviceprovide

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -9,15 +9,15 @@ class AppServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        // Force HTTPS di production
-        if ($this->app->environment('production')) {
-            URL::forceScheme('https');
-        }
+        // // Force HTTPS di production
+        // if ($this->app->environment('production')) {
+        //     URL::forceScheme('https');
+        // }
 
-        // Force root URL
-        if (config('app.url')) {
-            URL::forceRootUrl(config('app.url'));
-        }
+        // // Force root URL
+        // if (config('app.url')) {
+        //     URL::forceRootUrl(config('app.url'));
+        // }
     }
 
     public function register(): void
